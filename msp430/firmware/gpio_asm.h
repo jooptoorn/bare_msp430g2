ede433cd21e8b8e00d31bde39e68350fdccf20e5
Joop <jooptoorn@gmail.com>
2021-09-12 09:44:22 +0200

    unimportant changes

--- msp430/firmware/gpio_asm.h
+++ msp430/firmware/gpio_asm.h
@@ -1,4 +1,5 @@
 #define __DBL_MIN_EXP__ (-1021)
+#define __FLT32X_MAX_EXP__ 1024
 #define __UINT_LEAST16_MAX__ 0xffffU
 #define __ATOMIC_ACQUIRE 2
 #define __FLT_MIN__ 1.1754943508222875e-38F
@@ -13,6 +14,7 @@
 #define WDT_NMI_RISE_EDGE (0 << 6)
 #define __UINT8_MAX__ 0xff
 #define __WINT_MAX__ 0xffffU
+#define __FLT32_MIN_EXP__ (-125)
 #define NO_TRAMPOLINES 1
 #define __ORDER_LITTLE_ENDIAN__ 1234
 #define __SIZE_MAX__ 0xffffU
@@ -20,7 +22,9 @@
 #define __DBL_DENORM_MIN__ ((double)4.9406564584124654e-324L)
 #define __GCC_ATOMIC_CHAR_LOCK_FREE 1
 #define __GCC_IEC_559 0
+#define __FLT32X_DECIMAL_DIG__ 17
 #define __FLT_EVAL_METHOD__ 0
+#define __FLT64_DECIMAL_DIG__ 17
 #define gpio_write_pin(port,pin,val) (val ? gpio_write_pin_high(port,pin) : gpio_write_pin_low(port,pin))
 #define __GCC_ATOMIC_CHAR32_T_LOCK_FREE 1
 #define GPIO_PORT_LOW (0 )
@@ -28,11 +32,13 @@
 #define __SIG_ATOMIC_TYPE__ int
 #define __DBL_MIN_10_EXP__ (-307)
 #define __FINITE_MATH_ONLY__ 0
-#define __GNUC_PATCHLEVEL__ 0
+#define __GNUC_PATCHLEVEL__ 1
+#define __FLT32_HAS_DENORM__ 1
 #define __UINT_FAST8_MAX__ 0xffffU
 #define __has_include(STR) __has_include__(STR)
 #define __DEC64_MAX_EXP__ 385
 #define __INT8_C(c) c
+#define __INT_LEAST8_WIDTH__ 8
 #define __UINT_LEAST64_MAX__ 0xffffffffffffffffULL
 #define __SHRT_MAX__ 0x7fff
 #define __LDBL_MAX__ 1.7976931348623157e+308L
@@ -42,6 +48,7 @@
 #define __UINTMAX_TYPE__ long long unsigned int
 #define __DEC32_EPSILON__ 1E-6DF
 #define __MSP430XGENERIC__ 1
+#define __FLT_EVAL_METHOD_TS_18661_3__ 0
 #define __CHAR_UNSIGNED__ 1
 #define __UINT32_MAX__ 0xffffffffUL
 #define GPIO_OUTPUT (1)
@@ -49,6 +56,7 @@
 #define gpio_write_port_high(port) (port->OUT = 0xff)
 #define __WINT_MIN__ 0U
 #define _TYPES_ 
+#define __INT_LEAST16_WIDTH__ 16
 #define __SCHAR_MAX__ 0x7f
 #define __WCHAR_MIN__ (-__WCHAR_MAX__ - 1)
 #define __INT64_C(c) c ## LL
@@ -61,25 +69,34 @@
 #define __STDC_HOSTED__ 1
 #define __LDBL_HAS_INFINITY__ 1
 #define _FIRMWARE_ 
+#define __FLT32_DIG__ 6
 #define WDT_CLK_SM (0 << 2)
 #define __FLT_EPSILON__ 1.1920928955078125e-7F
+#define __SHRT_WIDTH__ 16
 #define __MSP430__ 1
 #define __LDBL_MIN__ 2.2250738585072014e-308L
 #define __DEC32_MAX__ 9.999999E96DF
+#define __FLT32X_HAS_INFINITY__ 1
 #define __INT32_MAX__ 0x7fffffffL
+#define __INT_WIDTH__ 16
 #define __SIZEOF_LONG__ 4
 #define __UINT16_C(c) c ## U
+#define __PTRDIFF_WIDTH__ 16
 #define _TIMER_ 
 #define __DECIMAL_DIG__ 17
 #define WDT_CLK_EXT (1 << 2)
+#define __FLT64_EPSILON__ 2.2204460492503131e-16F64
+#define __INTMAX_WIDTH__ 64
 #define __has_include_next(STR) __has_include_next__(STR)
 #define __LDBL_HAS_QUIET_NAN__ 1
+#define __FLT64_MANT_DIG__ 53
 #define gpio_set_dir_pin_output(port,pin) (port->DIR |= pin)
 #define GPIO_SEL2 ((GPIO_SEL2_HW_STRUCT*) 0x040)
-#define __GNUC__ 5
+#define __GNUC__ 8
 #define __FLT_HAS_DENORM__ 1
 #define __SIZEOF_LONG_DOUBLE__ 8
 #define __BIGGEST_ALIGNMENT__ 2
+#define __FLT64_MAX_10_EXP__ 308
 #define GPIO_PIN0 (1 << 0)
 #define GPIO_PIN1 (1 << 1)
 #define GPIO_PIN2 (1 << 2)
@@ -93,6 +110,8 @@
 #define __INT_FAST32_MAX__ 0x7fffffffL
 #define __DBL_HAS_INFINITY__ 1
 #define __DEC32_MIN_EXP__ (-94)
+#define __INTPTR_WIDTH__ 16
+#define __FLT32X_HAS_DENORM__ 1
 #define __INT_FAST16_TYPE__ int
 #define WDT_PW (0x5a << 8)
 #define __LDBL_HAS_DENORM__ 1
@@ -103,38 +122,52 @@
 #define WDT_PIN_MODE_NMI (1 << 5)
 #define gpio_write_pin_high(port,pin) (port->OUT |= pin)
 #define __DBL_MAX_EXP__ 1024
+#define __WCHAR_WIDTH__ 32
+#define __FLT32_MAX__ 3.4028234663852886e+38F32
 #define __DEC128_EPSILON__ 1E-33DL
 #define gpio_read_pin(port,pin) (port->IN & pin)
 #define __PTRDIFF_MAX__ 0x7fff
+#define __FLT32_HAS_QUIET_NAN__ 1
 #define __LONG_LONG_MAX__ 0x7fffffffffffffffLL
 #define __SIZEOF_SIZE_T__ 2
 #define __SIZEOF_WINT_T__ 2
-#define __GXX_ABI_VERSION 1009
+#define __LONG_LONG_WIDTH__ 64
+#define __FLT32_MAX_EXP__ 128
+#define __GXX_ABI_VERSION 1013
 #define __FLT_MIN_EXP__ (-125)
 #define gpio_write_pin_low(port,pin) (port->OUT &= ~pin)
 #define WDT_MODE_WDOG (0 << 4)
 #define __INT_FAST64_TYPE__ long long int
+#define __FLT64_DENORM_MIN__ 4.9406564584124654e-324F64
 #define __DBL_MIN__ ((double)2.2250738585072014e-308L)
 #define GPIO_INPUT (0)
+#define __FLT32X_EPSILON__ 2.2204460492503131e-16F32x
+#define __FLT64_MIN_EXP__ (-1021)
+#define __FLT64_MIN_10_EXP__ (-307)
 #define __DEC128_MIN__ 1E-6143DL
 #define __REGISTER_PREFIX__ 
 #define __UINT16_MAX__ 0xffffU
 #define __DBL_HAS_DENORM__ 1
+#define __FLT32_MIN__ 1.1754943508222875e-38F32
 #define __UINT8_TYPE__ unsigned char
 #define __NO_INLINE__ 1
 #define __FLT_MANT_DIG__ 24
+#define __LDBL_DECIMAL_DIG__ 17
 #define WDT_CLK_DIV_64 (3)
-#define __VERSION__ "5.3.0"
+#define __VERSION__ "8.3.1"
 #define __UINT64_C(c) c ## ULL
 #define __GCC_ATOMIC_INT_LOCK_FREE 1
+#define __FLT32_MANT_DIG__ 24
 #define __FLOAT_WORD_ORDER__ __ORDER_LITTLE_ENDIAN__
 #define __MSP430X__ 1
 #define gpio_read_port(port) (port->IN)
 #define gpio_set_dir_port(port,dir) (port->DIR = dir)
+#define __SCHAR_WIDTH__ 8
 #define __INT32_C(c) c ## L
 #define __DEC64_EPSILON__ 1E-15DD
 #define __ORDER_PDP_ENDIAN__ 3412
 #define __DEC128_MIN_EXP__ (-6142)
+#define __FLT32_MAX_10_EXP__ 38
 #define __INT_FAST32_TYPE__ long int
 #define gpio_set_dir_pin(port,pin,dir) (dir ? gpio_set_dir_pin_output(port,pin) : gpio_set_dir_pin_input(port,pin))
 #define __UINT_LEAST16_TYPE__ short unsigned int
@@ -151,8 +184,11 @@
 #define gpio_write_port(port,val) (val ? gpio_write_port_high(port) : gpio_write_port_low(port))
 #define __GCC_ATOMIC_WCHAR_T_LOCK_FREE 1
 #define __SIZEOF_PTRDIFF_T__ 2
+#define __FLT32X_MANT_DIG__ 53
+#define __FLT32X_MIN_EXP__ (-1021)
 #define __DEC32_SUBNORMAL_MIN__ 0.000001E-95DF
 #define __INT_FAST16_MAX__ 0x7fff
+#define __FLT64_DIG__ 15
 #define __UINT_FAST32_MAX__ 0xffffffffUL
 #define __UINT_LEAST64_TYPE__ long long unsigned int
 #define __FLT_HAS_QUIET_NAN__ 1
@@ -162,23 +198,31 @@
 #define __FLT_HAS_INFINITY__ 1
 #define __UINT_FAST16_TYPE__ unsigned int
 #define __DEC64_MAX__ 9.999999999999999E384DD
+#define __INT_FAST32_WIDTH__ 32
 #define NULL ((void*)0)
 #define __CHAR16_TYPE__ short unsigned int
 #define __PRAGMA_REDEFINE_EXTNAME 1
+#define __SIZE_WIDTH__ 16
 #define __INT_LEAST16_MAX__ 0x7fff
 #define __DEC64_MANT_DIG__ 16
 #define __INT64_MAX__ 0x7fffffffffffffffLL
 #define __UINT_LEAST32_MAX__ 0xffffffffUL
+#define __FLT32_DENORM_MIN__ 1.4012984643248171e-45F32
 #define __GCC_ATOMIC_LONG_LOCK_FREE 1
+#define __SIG_ATOMIC_WIDTH__ 16
 #define __INT_LEAST64_TYPE__ long long int
 #define __INT16_TYPE__ short int
 #define __INT_LEAST8_TYPE__ signed char
 #define __DEC32_MAX_EXP__ 97
 #define __INT_FAST8_MAX__ 0x7fff
 #define __INTPTR_MAX__ 0x7fff
+#define __FLT64_HAS_QUIET_NAN__ 1
+#define __FLT32_MIN_10_EXP__ (-37)
+#define __FLT32X_DIG__ 15
 #define WDT_PIN_MODE_RST (0 << 5)
 #define __LDBL_MANT_DIG__ 53
 #define __DBL_HAS_QUIET_NAN__ 1
+#define __FLT64_HAS_INFINITY__ 1
 #define false 0
 #define __SIG_ATOMIC_MIN__ (-__SIG_ATOMIC_MAX__ - 1)
 #define _CPU_ 
@@ -188,10 +232,13 @@
 #define __WCHAR_TYPE__ long int
 #define __SIZEOF_FLOAT__ 4
 #define __UINTPTR_MAX__ 0xffffU
+#define __INT_FAST64_WIDTH__ 64
 #define __DEC64_MIN_EXP__ (-382)
+#define __FLT32_DECIMAL_DIG__ 9
 #define __INT_FAST64_MAX__ 0x7fffffffffffffffLL
 #define __GCC_ATOMIC_TEST_AND_SET_TRUEVAL 1
 #define __FLT_DIG__ 6
+#define __FLT32_HAS_INFINITY__ 1
 #define IFG1 ((uint16_t*) 0x0002)
 #define __UINT_FAST64_TYPE__ long long unsigned int
 #define __INT_MAX__ 0x7fff
@@ -206,15 +253,19 @@
 #define __SIZEOF_SHORT__ 2
 #define __LDBL_MIN_EXP__ (-1021)
 #define gpio_write_port_low(port) (port->OUT = 0x0)
+#define __FLT64_MAX__ 1.7976931348623157e+308F64
 #define _MEMORYMAP_ 
 #define WDT_CLK_DIV_32768 (0)
+#define __WINT_WIDTH__ 16
 #define __INT_LEAST8_MAX__ 0x7f
+#define __FLT32X_MAX_10_EXP__ 308
 #define __LDBL_MAX_10_EXP__ 308
 #define __ATOMIC_RELAXED 0
 #define __DBL_EPSILON__ ((double)2.2204460492503131e-16L)
 #define __SIZEOF_INT20__ 4
 #define WDT_NMI_FALL_EDGE (1 << 6)
 #define __UINT8_C(c) c
+#define __FLT64_MAX_EXP__ 1024
 #define __INT_LEAST32_TYPE__ long int
 #define __SIZEOF_WCHAR_T__ 4
 #define __UINT64_TYPE__ long long unsigned int
@@ -223,10 +274,14 @@
 #define __INT_FAST8_TYPE__ int
 #define WDT_MODE_ITVL (1 << 4)
 #define __GNUC_STDC_INLINE__ 1
+#define __FLT64_HAS_DENORM__ 1
+#define __FLT32_EPSILON__ 1.1920928955078125e-7F32
 #define WDT_CLK_DIV_512 (2)
 #define IE1 ((uint16_t*) 0x0000)
 #define __DBL_DECIMAL_DIG__ 17
+#define __INT_FAST8_WIDTH__ 16
 #define __DEC_EVAL_METHOD__ 2
+#define __FLT32X_MAX__ 1.7976931348623157e+308F32x
 #define GPIO_EDGE_LOWTOHIGH (0)
 #define WDT_CTL ((uint16_t*) 0x0120)
 #define __ORDER_BIG_ENDIAN__ 4321
@@ -236,18 +291,24 @@
 #define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
 #define __FLT_DENORM_MIN__ 1.4012984643248171e-45F
 #define __INT8_MAX__ 0x7f
+#define __LONG_WIDTH__ 32
 #define __UINT_FAST32_TYPE__ long unsigned int
 #define __CHAR32_TYPE__ long unsigned int
 #define __FLT_MAX__ 3.4028234663852886e+38F
 #define __INT32_TYPE__ long int
 #define __SIZEOF_DOUBLE__ 8
 #define __FLT_MIN_10_EXP__ (-37)
+#define __FLT64_MIN__ 2.2250738585072014e-308F64
+#define __INT_LEAST32_WIDTH__ 32
 #define __INTMAX_TYPE__ long long int
 #define __DEC128_MAX_EXP__ 6145
+#define __FLT32X_HAS_QUIET_NAN__ 1
 #define __ATOMIC_CONSUME 1
 #define __GNUC_MINOR__ 3
+#define __INT_FAST16_WIDTH__ 16
 #define __UINTMAX_MAX__ 0xffffffffffffffffULL
 #define __DEC32_MANT_DIG__ 7
+#define __FLT32X_DENORM_MIN__ 4.9406564584124654e-324F32x
 #define __DBL_MAX_10_EXP__ 308
 #define __LDBL_DENORM_MIN__ 4.9406564584124654e-324L
 #define __INT16_C(c) c
@@ -256,6 +317,7 @@
 #define __PTRDIFF_TYPE__ int
 #define __ATOMIC_SEQ_CST 5
 #define __UINT32_TYPE__ long unsigned int
+#define __FLT32X_MIN_10_EXP__ (-307)
 #define __UINTPTR_TYPE__ unsigned int
 #define __DEC64_SUBNORMAL_MIN__ 0.000000000000001E-383DD
 #define __DEC128_MANT_DIG__ 34
@@ -263,11 +325,13 @@
 #define __SIZEOF_LONG_LONG__ 8
 #define __GCC_ATOMIC_LLONG_LOCK_FREE 1
 #define GEN_ASS_HDR 1
+#define __FLT32X_MIN__ 2.2250738585072014e-308F32x
 #define __LDBL_DIG__ 15
 #define __FLT_DECIMAL_DIG__ 9
 #define __UINT_FAST16_MAX__ 0xffffU
 #define __GCC_ATOMIC_SHORT_LOCK_FREE 1
 #define GPIO_EDGE_HIGHTOLOW (1)
+#define __INT_LEAST64_WIDTH__ 64
 #define __UINT_FAST8_TYPE__ unsigned int
 #define __ATOMIC_ACQ_REL 4
 #define __ATOMIC_RELEASE 3
